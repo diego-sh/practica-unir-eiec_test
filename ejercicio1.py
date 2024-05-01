a42e7363cd30a13bcd60d94b86f86bf5ecabfdef
ALTIORACORP\dalpala <dalpala@altioracorp.com>
2024-04-30 20:31:51 -0500

    add age

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,3 +3,6 @@ nombre = input()
 print("Por favor ingrese su apellido:")
 apellido = input()
 print(f"Sus datos fueron ingresados, gracias {nombre} {apellido}")
+
+edad = input()
+print(f"Su edad es {edad} años")
